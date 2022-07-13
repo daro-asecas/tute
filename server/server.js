583420bb2a9e7e0e045522f58a4a0bf638b03dba
Daro Asecas <daro.1919@gmail.com>
2022-07-12 23:37:04 -0300

    arreglando el port?

--- server/server.js
+++ server/server.js
@@ -38,6 +38,6 @@ server.on("error", (err) => {
   console.error("Server error: ", err);
 });
 
-server.listen(8000, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log("RPS started on 8000");
 });
