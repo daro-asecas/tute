83b7350fbba5e10314d0aaebc99fcf7ac6104410
Daro Asecas <daro.1919@gmail.com>
2022-07-14 15:22:20 -0300

    ahoraSi!

--- client/src/connection.js
+++ client/src/connection.js
@@ -1,7 +1,7 @@
 //  rebusque para que funcione localmente y onlinemente sin modificar los archivos
 
-const serverURL = (window.location.href=="http://localhost:8000")
-  ? "https://tute-online.herokuapp.com/"
-  : "http://localhost:8000"
+const serverURL = (window.location.href=="http://localhost:8000" || "http://localhost:8000/")
+  ? "http://localhost:8000"
+  : "https://tute-online.herokuapp.com/"
 
   const sock = io(serverURL);
