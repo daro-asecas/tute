4b0760ac47f63fb967a70f0cbe9d14316b7b2aac
Daro Asecas <daro.1919@gmail.com>
2022-07-14 15:58:51 -0300

    ahoraSi!

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const cors = require("cors");
 
 const Match = require("./models/match");
 
@@ -10,13 +11,13 @@ const clientPath = `${__dirname}/../client`;
 
 app.use(express.static(clientPath));
 
+app.use(cors({
+  origin: ["https://tute-online.netlify.app/", "localhost:8000"]
+}))
+
 const server = http.createServer(app);
 
-const io = socketio(server, {
-  cors: {
-    origin: ["https://tute-online.netlify.app/", "localhost:8000"]
-  }
-});
+const io = socketio(server);
 
 let waitingPlayer = null;
 
