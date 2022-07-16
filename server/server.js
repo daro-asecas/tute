61b6c837bf9b5d4181944a5db0872b106387c11e
Daro Asecas <daro.1919@gmail.com>
2022-07-16 13:07:17 -0300

    ahoraSi!

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = socketio(server, {
   cors: {
-    origin: "*",  // [https://tute-online.netlify.app", "localhost:8000"],
+    origin: ["https://tute-online.netlify.app", "localhost:8000"],
   }
 });
 
