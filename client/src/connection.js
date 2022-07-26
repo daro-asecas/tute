2933a4b6d495a714597df71abb4df06d645ec3e7
Daro Asecas <daro.1919@gmail.com>
2022-07-26 19:54:30 -0300

    multisala

--- client/src/connection.js
+++ client/src/connection.js
@@ -1,24 +1,10 @@
 // Rebusque para que funcione localmente y onlinemente sin modificar los archivos
 
-// const serverURL = (window.location.href.slice(0, 21)==="http://localhost:5000")
-//   ? "http://localhost:5000"
-//   : "https://tute-online.herokuapp.com/"
-
-// si esto esta superado se puede eliminar
-// let serverURL;
-// if (process && !!process.env && !!process.env.SERVER_URL) { serverURL = process.env.SERVER_URL
-// } else { serverURL = "http://localhost:5000" }
-
-// const serverURL = (process && process.env && process.env.SERVER_URL || "http://localhost:5000")
+// const serverURL = (window.location.href.slice(0, 21)==="http://localhost:5000") ? "http://localhost:5000" : "https://tute-online.herokuapp.com/"
 // const serverURL = (typeof(process)!="undefined" && typeof(process.env)!="undefined" && process.env.SERVER_URL!="undefined")? process.env.SERVER_URL : "http://localhost:5000"
 
-if(!serverURL) { const serverURL = "http://localhost:5000" }
-console.log(serverURL);
-
+if(!serverURL) { const serverURL = "http://localhost:5000" } // esto es porque en NETLIFY hay un SnippetInjection de esta variable!!
 const sock = io(serverURL);
-const titulo = document.querySelector("h1")
-titulo.innerHTML = `variable:${serverURL}`
-
 
 // Guardar nombre de usuario, por ahora lo dejo comentado para mas adelante
 // let userName = localStorage.userName;
