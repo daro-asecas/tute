0fa7a90237be7fd80a94d6cc2f6bc050b80902c2
Daro Asecas <daro.1919@gmail.com>
2022-08-18 00:21:37 -0300

    tute primera entrega

--- client/src/connection.js
+++ client/src/connection.js
@@ -3,10 +3,8 @@
 // const serverURL = (window.location.href.slice(0, 21)==="http://localhost:5000") ? "http://localhost:5000" : "https://tute-online.herokuapp.com/"
 // const serverURL = (typeof(process)!="undefined" && typeof(process.env)!="undefined" && process.env.SERVER_URL!="undefined")? process.env.SERVER_URL : "http://localhost:5000"
 
-// if(typeof(serverURL)=="undefined") { 
-//   console.log("entra2")
-//  const serverURL = "http://localhost:5000" // } // esto es porque en NETLIFY hay un SnippetInjection de esta variable!!
-  const sock = io(serverURL || "http://localhost:5000");
+const server = typeof(serverURL)!="undefined"?serverURL:"http://localhost:5000"
+const sock = io(server); // esto es porque en NETLIFY hay un SnippetInjection de esta variable!!
 
 // Guardar nombre de usuario, por ahora lo dejo comentado para mas adelante
 // let userName = localStorage.userName;
